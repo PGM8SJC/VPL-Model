21340cab8278ef00208d352673404ee6f23e12fc
ShahabBakht <shahab.bakhtiari@gmail.com>
2019-01-21 17:14:50 -0500

    added vector fields to generated stimulus

--- generateStimulus.m
+++ generateStimulus.m
@@ -1,10 +1,10 @@
-function generateStimulus()
+function [dotsPosition,motionVectors] = generateStimulus()
 
 ScreenSize      = [500,500];    % pixels    
 dotSize         = [1];          % pixels
 dotDensity      = [0.01];       % number of dots per pixel^2
 speed           = [100];          % pixel/sec
-direction       = [pi/2];          % radian
+direction       = [pi];          % radian
 apertureDiam    = [100];
 apertureLoc     = [350,150];
 coherence       = [1];          % portion of dots moving together
@@ -16,7 +16,7 @@ numDots = floor(apertureDiam * apertureDiam * dotDensity); % assuming that the a
 
 dotsInitialPosition = [randperm(apertureDiam,numDots) + apertureLoc(1) - apertureDiam/2;randperm(apertureDiam,numDots)+ apertureLoc(2) - apertureDiam/2];
 initialLife = rand(1,numDots) * lifeTime;
-
+initialMotionVector = repmat([speed;direction],1,numDots);
 numFramesNeeded = floor(duration * framerate / 1000);
 
 for framecount = 1:numFramesNeeded
@@ -25,11 +25,12 @@ for framecount = 1:numFramesNeeded
     if framecount == 1
         dotsPosition = dotsInitialPosition;
         dotsLife = initialLife;
+        motionVectors = initialMotionVector;
     else
         dotsPosition(1,:,framecount) = floor(dotsPosition(1,:,framecount-1) + (1/framerate) * speed * cos(direction));
         dotsPosition(2,:,framecount) = floor(dotsPosition(2,:,framecount-1) + (1/framerate) * speed * sin(direction));
         dotsLife = dotsLife + (1/framerate) * 1000;
-        
+        motionVectors(:,:,framecount) = repmat([speed;direction],1,numDots);
     end
     
     % relocate the dead dots
@@ -37,6 +38,11 @@ for framecount = 1:numFramesNeeded
     numDeadDots = sum(deadDotsIdx);
     dotsPosition(:,deadDotsIdx,framecount) = [randperm(apertureDiam,numDeadDots) + apertureLoc(1) - apertureDiam/2;randperm(apertureDiam,numDeadDots)+ apertureLoc(2) - apertureDiam/2];
     dotsLife(deadDotsIdx) = 0;
+    
+    % set the motion vector for deadDots to zero
+    if numDeadDots > 0
+        motionVectors(:,deadDotsIdx,framecount) = repmat([0;direction],1,numDeadDots);
+    end
     
     % replace the dots at the aperture border
     currentDotsPosition = squeeze(dotsPosition(:,:,end));
@@ -53,12 +59,20 @@ for framecount = 1:numFramesNeeded
     
 end
 
-for i = 1:30
-    figure;
-    I = zeros(500,500);
-    idx = sub2ind(size(I), [squeeze(dotsPosition(1,:,i))], [squeeze(dotsPosition(2,:,i))]);
-    I(idx) = 1;
-    figure(1);imagesc(I);colormap(gray);pause(1/framerate)
+% show the simulated stimulus
+% for i = 1:numFramesNeeded
+% %     figure;
+%     I = zeros(500,500);
+%     idx = sub2ind(size(I), [squeeze(dotsPosition(1,:,i))], [squeeze(dotsPosition(2,:,i))]);
+%     I(idx) = 1;
+%     figure(1);imagesc(I);colormap(gray);pause(1/framerate)
+% end
+
+for i = 1:numFramesNeeded
+    figure(1);imagesc(zeros(500,500));colormap(gray);hold on;
+    figure(1);quiver(squeeze(dotsPosition(1,:,i)),squeeze(dotsPosition(2,:,i)),motionVectors(1,:,i) .* cos(motionVectors(2,:,i)),motionVectors(1,:,i) .* sin(motionVectors(2,:,i)),'Color','k');
+    pause(1/framerate)
 end
+
 
 end
