d9c0ccf36671056121440ca9f25d95c89c15ee70
ShahabBakht <shahab.bakhtiari@gmail.com>
2019-01-24 15:31:44 -0500

    added coherence to generateStimulus

--- setStimulusParameters.m
+++ setStimulusParameters.m
@@ -4,11 +4,11 @@ M.screenSize      = [500,500];    % pixels
 M.dotSize         = [1];          % pixels
 M.dotDensity      = [0.01];       % number of dots per pixel^2
 M.speed           = [100];          % pixel/sec
-M.direction       = [pi];          % radian
+M.direction       = [pi/2];          % radian
 M.apertureDiam    = [100];
 M.apertureLoc     = [350,150];
-M.coherence       = [1];          % portion of dots moving together
-M.lifeTime        = [500];        % ms
+M.coherence       = [.8];          % portion of dots moving together
+M.lifeTime        = [200];        % ms
 M.duration        = [500];        % ms
 M.framerate        = 60;
 
